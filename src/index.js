0c4cddb7bd2c1de2b59744db3185248969dab672
Vitnin <chaos_stotch@proton.me>
2023-02-28 12:59:29 -0300

    gh pages changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {HashRouter } from "react-router-dom";
 import './index.css';
+
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    Hello world
-  </React.StrictMode>
+  <HashRouter>
+    <Routes>
+    <Route exact path ="/" element={
+      <div>
+        Hello
+      </div>
+    } />
+    </Routes>
+  </HashRouter>
 );
