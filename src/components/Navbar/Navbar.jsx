9f41f48507ba03fbcbe5719353f2b036b4d3c91a
Vitnin <chaos_stotch@proton.me>
2023-02-28 17:27:17 -0300

    navbar mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css'
 import LOGOIMG from '../../images/logo.png'
 import LANGUAGE1 from '../../images/usa.png'
@@ -6,14 +6,20 @@ import LANGUAGE1 from '../../images/usa.png'
 import {BsSearch} from 'react-icons/bs'
 
 function Navbar() {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
   return (
     <nav className='navbar'>
         <a href="/Bitcoin-Explorer" className='logo__link'>
           <img src={LOGOIMG} alt="logo" className='logo' />
         </a>
         <div className="search__bar">
-          <input type="text" placeholder='Block, Transaction or Address'/>
-          <button>
+          <input type="text" placeholder='Block, Transaction or Address' className={isActive ? 'input__active': null} />
+          <button onClick={toggleClass}>
             <BsSearch />
           </button>
         </div>
